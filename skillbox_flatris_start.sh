30a301128bb9b9c406bb38a0e55314a1ae47ed81
htalitha <75842695+htalitha@users.noreply.github.com>
2020-12-11 20:29:44 +0500

    Update skillbox_flatris_start.sh

--- skillbox_flatris_start.sh
+++ skillbox_flatris_start.sh
@@ -3,8 +3,9 @@
 sudo apt update
 sudo apt install docker.io docker-compose
 sudo gpasswd -a $USER docker
-git clone https://github.com/htalitha/skillbox.git
-cd skillbox
+git clone https://github.com/htalitha/skillbox.git ~/skillbox
+cd ~/skillbox
 git clone https://github.com/htalitha/flatris.git
-echo "@reboot /home/$USER/skillbox_flatris_init.sh" >> /etc/crontab
+crontab -l > file 2>/dev/null; echo "@reboot /home/$USER/skillbox/skillbox_flatris_init.sh" >> file; crontab file; rm file
+sudo /etc/init.d/cron restart
 sudo reboot
